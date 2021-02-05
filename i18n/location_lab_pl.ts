043ef146b87c197248fa610b08c7c73b61c3fdff
Hubert Zakoscielny <hz@gis-support.pl>
2021-02-05 22:34:46 +0100

    Handled HERE API url error, updated translations

--- i18n/location_lab_pl.ts
+++ i18n/location_lab_pl.ts
@@ -3,27 +3,27 @@
 <context>
     <name>Catchments</name>
     <message>
-        <location filename="../modules/catchments/main.py" line="116"/>
+        <location filename="../modules/catchments/main.py" line="117"/>
         <source>Car</source>
         <translation>Samochód</translation>
     </message>
     <message>
-        <location filename="../modules/catchments/main.py" line="116"/>
+        <location filename="../modules/catchments/main.py" line="117"/>
         <source>Pedestrian</source>
         <translation>Pieszo</translation>
     </message>
     <message>
-        <location filename="../modules/catchments/main.py" line="116"/>
+        <location filename="../modules/catchments/main.py" line="117"/>
         <source>Truck</source>
         <translation>Samochód ciężarowy</translation>
     </message>
     <message>
-        <location filename="../modules/catchments/main.py" line="116"/>
+        <location filename="../modules/catchments/main.py" line="117"/>
         <source>Bike</source>
         <translation>Rower</translation>
     </message>
     <message>
-        <location filename="../modules/catchments/main.py" line="74"/>
+        <location filename="../modules/catchments/main.py" line="75"/>
         <source>Minutes</source>
         <translation>Minuty</translation>
     </message>
@@ -33,155 +33,160 @@
         <translation>Metry</translation>
     </message>
     <message>
-        <location filename="../modules/catchments/main.py" line="111"/>
+        <location filename="../modules/catchments/main.py" line="112"/>
         <source>Insert App ID:App Code or apiKey</source>
         <translation>App ID:App Code lub klucz API</translation>
     </message>
     <message>
-        <location filename="../modules/catchments/main.py" line="113"/>
+        <location filename="../modules/catchments/main.py" line="114"/>
         <source>Free Plan Directions (2.000 requests per day @40 requests per minute)</source>
         <translation>Darmowy plan pozwala na maksymalnie 40 punktów na minutę, 2000 zapytań dziennie</translation>
     </message>
     <message>
-        <location filename="../modules/catchments/main.py" line="122"/>
+        <location filename="../modules/catchments/main.py" line="123"/>
         <source>Insert API key</source>
         <translation>Podaj klucz API</translation>
     </message>
     <message>
-        <location filename="../modules/catchments/main.py" line="250"/>
+        <location filename="../modules/catchments/main.py" line="235"/>
         <source>catchments not found</source>
         <translation>nie znaleziono zasięgów</translation>
     </message>
     <message>
-        <location filename="../modules/catchments/main.py" line="261"/>
+        <location filename="../modules/catchments/main.py" line="246"/>
         <source>provider</source>
         <translation>api</translation>
     </message>
     <message>
-        <location filename="../modules/catchments/main.py" line="261"/>
+        <location filename="../modules/catchments/main.py" line="246"/>
         <source>mode</source>
         <translation>tryb</translation>
     </message>
     <message>
-        <location filename="../modules/catchments/main.py" line="261"/>
+        <location filename="../modules/catchments/main.py" line="246"/>
         <source>value</source>
         <translation>wartość</translation>
     </message>
     <message>
-        <location filename="../modules/catchments/main.py" line="261"/>
+        <location filename="../modules/catchments/main.py" line="246"/>
         <source>units</source>
         <translation>jednostki</translation>
     </message>
     <message>
-        <location filename="../modules/catchments/main.py" line="261"/>
+        <location filename="../modules/catchments/main.py" line="246"/>
         <source>lat</source>
         <translation>szerokość</translation>
     </message>
     <message>
-        <location filename="../modules/catchments/main.py" line="261"/>
+        <location filename="../modules/catchments/main.py" line="246"/>
         <source>lon</source>
         <translation>długość</translation>
     </message>
     <message>
-        <location filename="../modules/catchments/main.py" line="338"/>
+        <location filename="../modules/catchments/main.py" line="321"/>
         <source>Number of points:</source>
         <translation>Liczba punktów:</translation>
     </message>
     <message>
         <location filename="../modules/catchments/main.py" line="338"/>
         <source>(limit is 5)</source>
-        <translation>(max to 5)</translation>
+        <translation type="obsolete">(max to 5)</translation>
     </message>
     <message>
-        <location filename="../modules/catchments/main.py" line="356"/>
+        <location filename="../modules/catchments/main.py" line="339"/>
         <source>No geometry</source>
         <translation>Brak geometrii</translation>
     </message>
     <message>
-        <location filename="../modules/catchments/main.py" line="364"/>
+        <location filename="../modules/catchments/main.py" line="347"/>
         <source>Catchments not found</source>
         <translation>Nie znaleziono zasięgów</translation>
     </message>
     <message>
-        <location filename="../modules/catchments/main.py" line="370"/>
+        <location filename="../modules/catchments/main.py" line="353"/>
         <source>Invalid API key</source>
         <translation>Nieprawidłowy klucz API</translation>
     </message>
     <message>
-        <location filename="../modules/catchments/main.py" line="376"/>
+        <location filename="../modules/catchments/main.py" line="359"/>
         <source>These credentials do not authorize access</source>
         <translation>Podane dane nie upoważniają do dostępu</translation>
     </message>
     <message>
-        <location filename="../modules/catchments/main.py" line="382"/>
+        <location filename="../modules/catchments/main.py" line="365"/>
         <source>Openservice API daily quota reached or API key unauthorized</source>
         <translation>Przekroczono dzienny limit zapytań lub podano niepoprawny klucz API</translation>
+    </message>
+    <message>
+        <location filename="../modules/catchments/main.py" line="372"/>
+        <source>An error occured while executing HERE API request. Error Message: </source>
+        <translation>Podczas zapytania do HERE API wystąpił błąd. Komunikat: </translation>
     </message>
 </context>
 <context>
     <name>CatchmentsDialogBase</name>
     <message>
-        <location filename="../modules/catchments/catchments.ui" line="20"/>
+        <location filename="../modules/catchments/catchments.ui" line="26"/>
         <source>Catchments</source>
         <translation>Zasięgi</translation>
     </message>
     <message>
-        <location filename="../modules/catchments/catchments.ui" line="29"/>
+        <location filename="../modules/catchments/catchments.ui" line="35"/>
         <source>API Provider</source>
         <translation>API</translation>
     </message>
     <message>
-        <location filename="../modules/catchments/catchments.ui" line="39"/>
+        <location filename="../modules/catchments/catchments.ui" line="45"/>
         <source>Key</source>
         <translation>Klucz</translation>
     </message>
     <message>
-        <location filename="../modules/catchments/catchments.ui" line="77"/>
+        <location filename="../modules/catchments/catchments.ui" line="83"/>
         <source>Select point layer</source>
         <translation>Wybierz warstwę punktową</translation>
     </message>
     <message>
-        <location filename="../modules/catchments/catchments.ui" line="88"/>
+        <location filename="../modules/catchments/catchments.ui" line="94"/>
         <source>Select layer ID field</source>
         <translation>Wybierz pole z ID warstwy</translation>
     </message>
     <message>
-        <location filename="../modules/catchments/catchments.ui" line="104"/>
+        <location filename="../modules/catchments/catchments.ui" line="110"/>
         <source>Only selected</source>
         <translation>Tylko zaznaczone</translation>
     </message>
     <message>
-        <location filename="../modules/catchments/catchments.ui" line="114"/>
+        <location filename="../modules/catchments/catchments.ui" line="120"/>
         <source>Number of points: 0</source>
         <translation>Liczba punktów: 0</translation>
     </message>
     <message>
-        <location filename="../modules/catchments/catchments.ui" line="128"/>
+        <location filename="../modules/catchments/catchments.ui" line="134"/>
         <source>Mode</source>
         <translation>Tryb</translation>
     </message>
     <message>
-        <location filename="../modules/catchments/catchments.ui" line="138"/>
+        <location filename="../modules/catchments/catchments.ui" line="144"/>
         <source>Value and unit</source>
         <translation>Wartość i jednostka</translation>
     </message>
     <message>
-        <location filename="../modules/catchments/catchments.ui" line="155"/>
+        <location filename="../modules/catchments/catchments.ui" line="161"/>
         <source>Parameters</source>
         <translation>Parametry</translation>
     </message>
     <message>
-        <location filename="../modules/catchments/catchments.ui" line="165"/>
+        <location filename="../modules/catchments/catchments.ui" line="171"/>
         <source>Include traffic</source>
         <translation>Uwzględnij natężenie ruchu</translation>
     </message>
     <message>
-        <location filename="../modules/catchments/catchments.ui" line="175"/>
+        <location filename="../modules/catchments/catchments.ui" line="181"/>
         <source>Points as route start</source>
         <translation>Punkty jako początek trasy</translation>
     </message>
     <message>
-        <location filename="../modules/catchments/catchments.ui" line="201"/>
+        <location filename="../modules/catchments/catchments.ui" line="207"/>
         <source>Get catchments</source>
         <translation>Pobierz zasięgi</translation>
     </message>
@@ -215,17 +220,17 @@
 <context>
     <name>Geocoder</name>
     <message>
-        <location filename="../modules/geocoder/main.py" line="235"/>
+        <location filename="../modules/geocoder/main.py" line="236"/>
         <source>no addresses provided</source>
         <translation>nie podano żadnych adresów</translation>
     </message>
     <message>
-        <location filename="../modules/geocoder/main.py" line="201"/>
+        <location filename="../modules/geocoder/main.py" line="202"/>
         <source>Unknown error occurred</source>
         <translation>Wystąpił nieznany błąd</translation>
     </message>
     <message>
-        <location filename="../modules/geocoder/main.py" line="207"/>
+        <location filename="../modules/geocoder/main.py" line="208"/>
         <source>Save to...</source>
         <translation>Zapisz do...</translation>
     </message>
@@ -235,7 +240,7 @@
         <translation type="obsolete">Nieprawidłowy klucz API. Wprowadź poprawny klucz i spróbuj ponownie.</translation>
     </message>
     <message>
-        <location filename="../modules/geocoder/main.py" line="232"/>
+        <location filename="../modules/geocoder/main.py" line="233"/>
         <source>addresses computed</source>
         <translation>zageokodowanych adresów</translation>
     </message>
@@ -316,12 +321,12 @@
 <context>
     <name>GeocoderHERE</name>
     <message>
-        <location filename="../modules/geocoder/modules/here/geocoder.py" line="67"/>
+        <location filename="../modules/geocoder/modules/here/geocoder.py" line="66"/>
         <source>Geocoding successful</source>
         <translation>Pomyślnie ukończono geokodowanie</translation>
     </message>
     <message>
-        <location filename="../modules/geocoder/modules/here/geocoder.py" line="34"/>
+        <location filename="../modules/geocoder/modules/here/geocoder.py" line="33"/>
         <source>HERE batch geocode request is being created, it may take a while to complete.</source>
         <translation>Tworzone jest zapytanie do HERE API. Jego wykonanie może zająć trochę czasu.</translation>
     </message>
@@ -336,7 +341,7 @@
         <translation type="obsolete">Podczas pobierania odpowiedzi z HERE API wystąpił błąd.</translation>
     </message>
     <message>
-        <location filename="../modules/geocoder/modules/here/geocoder.py" line="31"/>
+        <location filename="../modules/geocoder/modules/here/geocoder.py" line="30"/>
         <source>Error: </source>
         <translation>Błąd: </translation>
     </message>
@@ -344,7 +349,7 @@
 <context>
     <name>GeocoderORS</name>
     <message>
-        <location filename="../modules/geocoder/modules/openrouteservice/geocoder.py" line="61"/>
+        <location filename="../modules/geocoder/modules/openrouteservice/geocoder.py" line="63"/>
         <source>Geocoding succesful</source>
         <translation>Pomyslnie ukończono geokodowanie</translation>
     </message>
@@ -356,7 +361,7 @@
     <message>
         <location filename="../modules/geocoder/modules/openrouteservice/geocoder.py" line="31"/>
         <source>Daily request limit reached or invalid API key provided.</source>
-        <translation>Przekroczono dzienny limit zapytań lub podano niepoprawny klucz API.</translation>
+        <translation type="obsolete">Przekroczono dzienny limit zapytań lub podano niepoprawny klucz API.</translation>
     </message>
 </context>
 <context>
